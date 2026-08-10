3cd417ef711e0e8a88c33c28f7a2fe82e89124f0
Javi <jcaboroca@gmail.com>
2026-08-10 15:36:55 +0200

    Conectar el backend de Apps Script

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 // Para compartir datos con el equipo, despliega backend/apps-script.gs como
 // aplicación web de Google Apps Script y pega aquí la URL /exec.
 window.CONFIG = {
-  API_URL: "",
+  API_URL: "https://script.google.com/macros/s/AKfycbyAhtlh3VK7AC39ohvhX7c5fds1QpKg3p5q9-TJ4KnNINRccCTqXI4RUF--4Edu7wejhA/exec",
 };
